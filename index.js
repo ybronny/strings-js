7569e69d86153033b042a40a42a2179ab840827e
ronny <ronnymwangi7@gmail.com>
2024-03-20 14:55:45 +0300

    Assignment completed

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-// Write your code in this file!
+const currentUser = "Grace Hopper"
+const welcomeMessage = "Welcome to Flatbook, " + "Grace Hopper!"
+const excitedWelcomeMessage = "WELCOME TO FLATBOOK, " + "GRACE HOPPER!"
+const shortGreeting =  "Welcome, " + "G!"
